22b43f8b8ccfc74180da3065ca7735091400e8c1
EdoardoGruppi <edoardo.gruppi.20@ucl.ac.uk>
2021-09-14 17:36:19 +0200

    ❌ Delete files not used

---
+++ src/Components/Row.jsx
@@ -0,0 +1,34 @@
+import React, { useEffect, useState } from "react";
+import "./Row.css";
+
+// Base url to get the images
+const baseImagesUrl = "https://image.tmdb.org/t/p/original/";
+
+export default function Row({ title, fetchUrl }) {
+  const [movies, setMovies] = useState([]);
+
+  useEffect(() => {
+    // With [], the function executes only once when the row loads. It does not run again.
+    // Otherwise it runs also every time the variable within [] changes.
+    fetch(fetchUrl)
+      .then((res) => res.json())
+      .then((data) => setMovies(data.results));
+  }, [fetchUrl]);
+
+  return (
+    <div className="row">
+      <h2>{title}</h2>
+      <div className="row_posters">
+        {movies.map((movie) => (
+          <img
+            // React uses keys to optimise the rendering. If anything changes in a Row, React renders only what needed
+            key={movie.id}
+            className="poster"
+            src={`${baseImagesUrl}${movie.poster_path}`}
+            alt={movie.name}
+          />
+        ))}
+      </div>
+    </div>
+  );
+}
