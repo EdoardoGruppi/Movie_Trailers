434d6e1ec0e207880ed588c52eb3a7f9e19440df
EdoardoGruppi <edoardo.gruppi.20@ucl.ac.uk>
2021-09-14 12:43:24 +0200

    💎 Reformat code

--- src/MovieTrailers/MovieTrailers.jsx
+++ src/MovieTrailers/MovieTrailers.jsx
@@ -1,47 +1,25 @@
 import { useState } from "react";
 import ReactPlayer from "react-player";
-import { getTrailerUrl } from "../Helpers/utilities";
 import "./MovieTrailers.css";
 import AutomaticSuggestions from "../Components/AutoSuggest";
 
 export default function MovieTrailers() {
   const [url, setUrl] = useState(null);
-  const [title, setTitle] = useState("");
   const [modal, setModal] = useState(false);
 
+  // Active overlay when required
   if (modal) document.body.classList.add("active-modal");
   else document.body.classList.remove("active-modal");
 
   return (
     <>
       <div>
-        <input
-          value={title}
-          type="text"
-          placeholder="Input a title"
-          onChange={(e) => setTitle(e.target.value)}
-        />
-        <button
-          className="btn-modal"
-          onClick={() => {
-            getTrailerUrl(title).then((data) => {
-              setUrl(data);
-              setModal(true);
-            });
-          }}
-        >
-          Search
-        </button>
-        <AutomaticSuggestions />
+        <AutomaticSuggestions setUrl={setUrl} setModal={setModal} />
         {modal && (
           <div className="modal">
             <div className="overlay" onClick={() => setModal(false)}>
               <div className="player-wrapper">
-                <ReactPlayer
-                  url={url}
-                  onChange={() => console.log(`Change ${url}`)}
-                  playing={true}
-                />
+                <ReactPlayer url={url} playing={true} />
                 <button className="close-modal" onClick={() => setModal(false)}>
                   Close
                 </button>
