f0ec250ca5619b9282f42ac0873a9f757d8c0fe7
EdoardoGruppi <edoardo.gruppi.20@ucl.ac.uk>
2021-09-14 12:42:27 +0200

    💎 Reformat code and add comment

--- src/Helpers/utilities.js
+++ src/Helpers/utilities.js
@@ -1,30 +1,30 @@
 import { apiKey, baseUrl } from "../Config";
 
-export async function getTrailerUrl(title) {
-  const { results } = await searchMovie(title);
-  //////////////////////////////////////////////////////
-  // Now we will consider only the first of the search
-  const videoDetails = await getVideoKey(results[0].id);
+export async function getTrailerUrl(film) {
+  // Get the youtube link to the trailer of the film
+  const videoDetails = await getVideoKey(film.id);
   const youTubeKey = videoDetails.results[0].key;
   return `https://www.youtube.com/watch?v=${youTubeKey}`;
 }
 
-export async function searchMovie(title) {
+async function searchMovie(title) {
+  // Perform the search of the movie through a specific url
   const url = `${baseUrl}/search/movie?${apiKey}&language=en-US&query=${title}&page=1`;
   return fetch(url).then((res) => res.json());
 }
 
-export async function getVideoKey(movieID) {
+async function getVideoKey(movieID) {
+  // Get the youtube video keys related to a film
   const url = `${baseUrl}/movie/${movieID}/videos?${apiKey}&language=en-US`;
   return fetch(url).then((res) => res.json());
 }
 
 export async function getFilmsSuggested(input, n_suggestions = 5) {
+  // Search movies whose title includes a certain string
   const { results } = await searchMovie(input);
+  // Get the list of the titles of the movie searched
   const suggestedTitles = results
-    .map((film) => {
-      return film.title;
-    })
+    .map((film) => film.title)
     .slice(0, n_suggestions);
-  return suggestedTitles;
+  return [results, suggestedTitles];
 }
