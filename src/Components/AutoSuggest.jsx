2a047fa4a6ee3804c57576b6629c28dd92d5676d
EdoardoGruppi <edoardo.gruppi.20@ucl.ac.uk>
2021-09-14 12:43:12 +0200

    💎 Reformat code and convert component towards React hooks style

--- src/Components/AutoSuggest.jsx
+++ src/Components/AutoSuggest.jsx
@@ -1,75 +1,50 @@
-import React from "react";
+import React, { useState } from "react";
 import Autosuggest from "react-autosuggest";
-import { getFilmsSuggested } from "../Helpers/utilities";
+import { getFilmsSuggested, getTrailerUrl } from "../Helpers/utilities";
 import "./AutoSuggest.css";
 
-// When suggestion is clicked, Autosuggest needs to populate the input
-// based on the clicked suggestion. Teach Autosuggest how to calculate the
-// input value for every given suggestion.
-const getSuggestionValue = (suggestion) => suggestion;
+export default function AutomaticSuggestions(params) {
+  const [value, setValue] = useState("");
+  const [filmsSuggested, setFilmsSuggested] = useState([]);
+  const [suggestions, setSuggestions] = useState([]);
 
-// Use your imagination to render suggestions.
-const renderSuggestion = (suggestion) => <div>{suggestion}</div>;
-
-export default class AutomaticSuggestions extends React.Component {
-  constructor() {
-    super();
-
-    // Autosuggest is a controlled component.
-    // This means that you need to provide an input value
-    // and an onChange handler that updates this value (see below).
-    // Suggestions also need to be provided to the Autosuggest,
-    // and they are initially empty because the Autosuggest is closed.
-    this.state = {
-      value: "",
-      suggestions: [],
-    };
-  }
-
-  onChange = (event, { newValue }) => {
-    this.setState({
-      value: newValue,
+  // Every time a character is inputted update the suggested films
+  const onSuggestionsFetchRequested = ({ value }) => {
+    getFilmsSuggested(value).then(([films, titles]) => {
+      setFilmsSuggested(films);
+      setSuggestions(titles);
     });
   };
 
-  // Autosuggest will call this function every time you need to update suggestions.
-  // You already implemented this logic above, so just use it.
-  onSuggestionsFetchRequested = ({ value }) => {
-    getFilmsSuggested(value).then((data) => {
-      const newState = data.length === 0 ? [] : data;
-      this.setState({ suggestions: newState });
-    });
+  const handleChanges = (event, { newValue }) => {
+    // Update the value state, i.e. the string inputted so far
+    setValue(newValue);
   };
 
-  // Autosuggest will call this function every time you need to clear suggestions.
-  onSuggestionsClearRequested = () => {
-    this.setState({
-      suggestions: [],
+  const getSuggestionValue = (title) => {
+    // When a suggestion is clicked update the input string and the parent states (url, modal)
+    const film = filmsSuggested.find((item) => item.title === title);
+    getTrailerUrl(film).then((data) => {
+      params.setUrl(data);
+      params.setModal(true);
     });
+    return title;
   };
 
-  render() {
-    const { value, suggestions } = this.state;
-
-    // Autosuggest will pass through all these props to the input.
-    const inputProps = {
-      placeholder: "Insert title",
-      value,
-      onChange: this.onChange,
-    };
-
-    // Finally, render it!
-    return (
-      <div>
-        <Autosuggest
-          suggestions={suggestions}
-          onSuggestionsFetchRequested={this.onSuggestionsFetchRequested}
-          onSuggestionsClearRequested={this.onSuggestionsClearRequested}
-          getSuggestionValue={getSuggestionValue}
-          renderSuggestion={renderSuggestion}
-          inputProps={inputProps}
-        />
-      </div>
-    );
-  }
+  return (
+    <div>
+      <Autosuggest
+        suggestions={suggestions}
+        onSuggestionsFetchRequested={onSuggestionsFetchRequested}
+        onSuggestionsClearRequested={() => setSuggestions([])}
+        getSuggestionValue={getSuggestionValue}
+        renderSuggestion={(suggestion) => <div>{suggestion}</div>}
+        inputProps={{
+          placeholder: "Insert a title",
+          value,
+          onChange: handleChanges,
+        }}
+      />
+    </div>
+  );
 }
