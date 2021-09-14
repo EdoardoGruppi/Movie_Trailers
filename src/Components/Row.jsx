d87f8af0300dc52cea75a00bd006c2c8055a4097
EdoardoGruppi <edoardo.gruppi.20@ucl.ac.uk>
2021-09-14 23:55:04 +0200

    🔨 Add player to visualize trailers when a poster is clicked

--- src/Components/Row.jsx
+++ src/Components/Row.jsx
@@ -1,9 +1,13 @@
 import React, { useEffect, useState } from "react";
 import "./Row.css";
 import { baseImagesUrl } from "../Config";
+import ReactPlayer from "react-player";
+import { getTrailerUrl } from "../Helpers/Utilities";
+import closeLogo from "../close.png";
 
 export default function Row({ title, fetchUrl }) {
   const [movies, setMovies] = useState([]);
+  const [trailerUrl, setTrailerUrl] = useState("");
 
   useEffect(() => {
     // With [], the function executes only once when the row loads. It does not run again.
@@ -13,6 +17,12 @@ export default function Row({ title, fetchUrl }) {
       .then((data) => setMovies(data.results));
   }, [fetchUrl]);
 
+  const handleClick = (movie) => {
+    getTrailerUrl(movie)
+      .then((url) => setTrailerUrl(url))
+      .catch((error) => console.log(error));
+  };
+
   return (
     <div className="row">
       <h2 className="title">{title}</h2>
@@ -21,12 +31,30 @@ export default function Row({ title, fetchUrl }) {
           <img
             // React uses keys to optimise the rendering. If anything changes in a Row, React renders only what needed
             key={movie.id}
+            onClick={() => handleClick(movie)}
             className="poster"
             src={`${baseImagesUrl}${movie.poster_path}`}
             alt={movie.name}
           />
         ))}
       </div>
+      {trailerUrl && (
+        <div className="modal">
+          <div className="overlay" onClick={() => setTrailerUrl("")}>
+            <div className="player-wrapper">
+              <ReactPlayer url={trailerUrl} playing={true} />
+              <img
+                src={closeLogo}
+                alt={
+                  "https://fontmeme.com/permalink/210914/f2752e7a5b43ecc7c518f9609c9587dd.png"
+                }
+                className="close-button"
+                onClick={() => setTrailerUrl("")}
+              />
+            </div>
+          </div>
+        </div>
+      )}
     </div>
   );
 }
