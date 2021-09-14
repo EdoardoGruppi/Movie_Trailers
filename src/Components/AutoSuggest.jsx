cf212ea0cda2e2e5e17ca352c641529fd8bd9de2
EdoardoGruppi <edoardo.gruppi.20@ucl.ac.uk>
2021-09-14 10:58:03 +0200

    🔨 Add autosuggest component to help the search

---
+++ src/Components/AutoSuggest.jsx
@@ -0,0 +1,75 @@
+import React from "react";
+import Autosuggest from "react-autosuggest";
+import { getFilmsSuggested } from "../Helpers/utilities";
+import "./AutoSuggest.css";
+
+// When suggestion is clicked, Autosuggest needs to populate the input
+// based on the clicked suggestion. Teach Autosuggest how to calculate the
+// input value for every given suggestion.
+const getSuggestionValue = (suggestion) => suggestion;
+
+// Use your imagination to render suggestions.
+const renderSuggestion = (suggestion) => <div>{suggestion}</div>;
+
+export default class AutomaticSuggestions extends React.Component {
+  constructor() {
+    super();
+
+    // Autosuggest is a controlled component.
+    // This means that you need to provide an input value
+    // and an onChange handler that updates this value (see below).
+    // Suggestions also need to be provided to the Autosuggest,
+    // and they are initially empty because the Autosuggest is closed.
+    this.state = {
+      value: "",
+      suggestions: [],
+    };
+  }
+
+  onChange = (event, { newValue }) => {
+    this.setState({
+      value: newValue,
+    });
+  };
+
+  // Autosuggest will call this function every time you need to update suggestions.
+  // You already implemented this logic above, so just use it.
+  onSuggestionsFetchRequested = ({ value }) => {
+    getFilmsSuggested(value).then((data) => {
+      const newState = data.length === 0 ? [] : data;
+      this.setState({ suggestions: newState });
+    });
+  };
+
+  // Autosuggest will call this function every time you need to clear suggestions.
+  onSuggestionsClearRequested = () => {
+    this.setState({
+      suggestions: [],
+    });
+  };
+
+  render() {
+    const { value, suggestions } = this.state;
+
+    // Autosuggest will pass through all these props to the input.
+    const inputProps = {
+      placeholder: "Insert title",
+      value,
+      onChange: this.onChange,
+    };
+
+    // Finally, render it!
+    return (
+      <div>
+        <Autosuggest
+          suggestions={suggestions}
+          onSuggestionsFetchRequested={this.onSuggestionsFetchRequested}
+          onSuggestionsClearRequested={this.onSuggestionsClearRequested}
+          getSuggestionValue={getSuggestionValue}
+          renderSuggestion={renderSuggestion}
+          inputProps={inputProps}
+        />
+      </div>
+    );
+  }
+}
