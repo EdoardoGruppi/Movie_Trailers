094d8fa96480181040b5076ca0c01aada6d9a2f1
EdoardoGruppi <edoardo.gruppi.20@ucl.ac.uk>
2021-09-15 14:32:48 +0200

    🔨 Add Matrix component to visualize posters in the SearchPage

---
+++ src/Components/Matrix.jsx
@@ -0,0 +1,51 @@
+import React, { useState } from "react";
+import "./Matrix.css";
+import "./Row.css";
+import { getTrailerUrl } from "../Helpers/Utilities";
+import { baseImagesUrl } from "../Helpers/Config";
+import closeLogo from "../Images/close.png";
+import ReactPlayer from "react-player";
+
+export default function Matrix({ movies }) {
+  const [trailerUrl, setTrailerUrl] = useState("");
+
+  const handleClick = (movie) => {
+    getTrailerUrl(movie)
+      .then((url) => setTrailerUrl(url))
+      .catch((error) => console.log(error));
+  };
+
+  return (
+    <div className="matrix">
+      <h2 className="title">Results</h2>
+      <div className="matrix-posters">
+        {movies.map((movie) => (
+          <img
+            key={movie.id}
+            onClick={() => handleClick(movie)}
+            className="matrix-poster"
+            src={`${baseImagesUrl}${movie.poster_path}`}
+            alt={movie.name}
+          />
+        ))}
+      </div>
+      {trailerUrl && (
+        <div className="modal">
+          <div className="overlay" onClick={() => setTrailerUrl("")}>
+            <div className="player-wrapper">
+              <ReactPlayer url={trailerUrl} playing={true} />
+              <img
+                src={closeLogo}
+                alt={
+                  "https://fontmeme.com/permalink/210914/f2752e7a5b43ecc7c518f9609c9587dd.png"
+                }
+                className="close-button"
+                onClick={() => setTrailerUrl("")}
+              />
+            </div>
+          </div>
+        </div>
+      )}
+    </div>
+  );
+}
