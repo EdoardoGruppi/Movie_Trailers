c3d75b518e9772b360bbe005e771889a1bfb5737
EdoardoGruppi <edoardo.gruppi.20@ucl.ac.uk>
2021-09-15 14:31:53 +0200

    🔨 Add Router component and functioning

--- src/App.js
+++ src/App.js
@@ -1,12 +1,19 @@
 import "./App.css";
 import React from "react";
 import MovieTrailers from "./MovieTrailers/MovieTrailers";
+import SearchPage from "./MovieTrailers/SearchPage";
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 
 function App() {
   return (
-    <div className="app">
-      <MovieTrailers></MovieTrailers>
-    </div>
+    <Router>
+      <div className="app">
+        <Switch>
+          <Route path="/" component={MovieTrailers} exact></Route>
+          <Route path="/search" component={SearchPage}></Route>
+        </Switch>
+      </div>
+    </Router>
   );
 }
 
