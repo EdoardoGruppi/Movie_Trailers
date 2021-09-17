897854e36044f77ceccf667c3fbb8f72b67ded58
EdoardoGruppi <edoardo.gruppi.20@ucl.ac.uk>
2021-09-17 10:09:32 +0200

    💎 Reformat code

--- src/Helpers/Config.js
+++ src/Helpers/Config.js
@@ -2,8 +2,3 @@ export const apiKey = `api_key=${process.env.REACT_APP_AUTH_KEY}`;
 export const baseUrl = "https://api.themoviedb.org/3";
 // Base url to get the images
 export const baseImagesUrl = "https://image.tmdb.org/t/p/original/";
-
-// require("dotenv").config();
-// if (process.env.NODE_ENV === "production") {
-//   console.log = function () {};
-// }
